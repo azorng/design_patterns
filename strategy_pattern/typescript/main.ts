209086b9ccfbae324f52a0217622943d10a860f8
Azor <i@azor.net>
2021-11-06 15:56:26 +0100

    Improve ts

--- strategy_pattern/typescript/main.ts
+++ strategy_pattern/typescript/main.ts
@@ -1,61 +1,83 @@
 interface Flyable {
-  fly(): void;
-}
-
-interface Quackable {
-  quack(): void;
+    fly(): void
 }
 
 class FlyWithWings implements Flyable {
-  fly() {
-    console.log("I am flying with wings");
-  }
+    fly() {
+        console.log('I am flying with wings')
+    }
 }
 
 class NoFly implements Flyable {
-  fly() {
-    console.log("I can not fly :(");
-  }
+    fly() {
+        console.log('I can not fly :(')
+    }
+}
+
+interface Quackable {
+    quack(): void
 }
 
 class Quack implements Quackable {
-  quack() {
-    console.log("QUAAACK");
-  }
+    quack() {
+        console.log('QUAAACK')
+    }
 }
 
 class NoQuack implements Quackable {
-  quack() {
-    console.log("I can not quack :(");
-  }
+    quack() {
+        console.log('I can not quack :(')
+    }
 }
 
 abstract class Duck {
-  protected flyBehavior?: Flyable;
-  protected quackBehavior?: Quackable;
-  abstract display(): void;
+    flyBehavior: Flyable
+    quackBehavior: Quackable
+    abstract display(): void
+    swim() {
+        console.log('All ducks can swim!')
+    }
 
-  fly() {
-    this.flyBehavior?.fly();
-  }
+    fly() {
+        this.flyBehavior.fly()
+    }
 
-  quack() {
-    this.quackBehavior?.quack();
-  }
+    quack() {
+        this.quackBehavior.quack()
+    }
 }
 
 class RubberDuck extends Duck {
-  constructor() {
-    super();
-    this.quackBehavior = new Quack();
-  }
+    constructor() {
+        super()
+        this.quackBehavior = new NoQuack()
+        this.flyBehavior = new NoFly()
+    }
 
-  display() {
-    console.log("Displays a rubber duck");
-  }
+    display() {
+        console.log('Displays a rubber duck')
+    }
 }
 
-const duck = new RubberDuck();
-duck.fly();
-duck.quack();
-duck.display();
+class MallardDuck extends Duck {
+    constructor() {
+        super()
+        this.quackBehavior = new Quack()
+        this.flyBehavior = new FlyWithWings()
+    }
+
+    display() {
+        console.log('Displays a rubber duck')
+    }
+}
+
+function main() {
+    for (const duck of [new RubberDuck(), new MallardDuck()]) {
+        duck.display()
+        duck.fly()
+        duck.quack()
+        duck.swim()
+    }
+}
+
+main()
